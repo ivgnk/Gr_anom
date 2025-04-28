252bdec100aa93b01dac09ef34b2a8893af89dcb
Ivan Genik <IVGenik@rambler.ru>
2025-04-28 19:03:57 +0500

    28.04.2025

--- gr_field1.py
+++ gr_field1.py
@@ -3,6 +3,8 @@ https://heybro.ai
 расчет гравитационного поля внутри и снаружи шара:
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import inspect
 
 # Константы
 G = 6.67430e-11  # гравитационная постоянная, м^3/(кг·с^2)
@@ -39,6 +41,10 @@ def tst_gravity_field():
 
 
 def gravity_field_vector(center, point):
+    """
+    Вычисление гравитационного поля при задании центра шара (center) и
+    точки вычисления (point) с помощью координат
+    """
     x0, y0, z0 = center
     x1, y1, z1 = point
 
@@ -68,6 +74,102 @@ def tst_gravity_field_vector():
     g_field = gravity_field_vector(center, point)
     print(f"Гравитационное поле в точке {point}: {g_field} м/с^2")
 
+
+
+# Примеры работы с классами в Python
+nxi = 2001  # число значений по x
+nyi = 2001  # число значений по y
+centeri=(0, 0, 0) # центр шара, метры
+xmini = -1000 # миним по Х, метры
+xmaxi = 1000 # максим по Х, метры
+ymini = 10 # миним по Y, метры
+ymaxi = 2000 # максим по Y, метры
+
+# https://pythonru.com/primery/primery-raboty-s-klassami-v-python
+class Grav_sphere():
+    """
+    Построение диаграммы: как меняется гравитационное поле на профиле по мере
+    удаления от шарообразного источника
+    """
+    sphere_count:int = 0
+    Calced: bool = False
+
+    def __init__(self, nx=nxi, ny=nyi, center=centeri,
+                 xmin=xmini, xmax=xmaxi,
+                 ymin=ymini, ymax=ymaxi):
+        """
+        Инициализация объекта
+        """
+        print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+        self.nx = nx
+        self.ny = ny
+        self.center=center
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+        Grav_sphere.sphere_count += 1
+        Grav_sphere.Calced = False
+
+    def print_ini_param(self):
+        """
+        Проверочный вывод для __init__
+        """
+        print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+        print(f'{self.nx=}')
+        print(f'{self.ny=}')
+        print(f'{self.center=}')
+        print(f'{self.xmin=}')
+        print(f'{self.xmax=}')
+        print(f'{self.ymin=}')
+        print(f'{self.ymax=}')
+        print(f'{Grav_sphere.sphere_count=}')
+
+    def make_coo(self):
+        """
+        Задание координат для расчетов
+        """
+        print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+        zer=np.zeros(self.nx)
+        ons=np.ones(self.nx)
+        self.x = np.linspace(self.xmin, self.xmax, self.nx) # х-координаты
+        self.y = np.linspace(self.ymin, self.ymax, self.ny) # сдвиг по y-координате
+        self.res=np.column_stack([self.x,ons, zer])
+        self.xn, self.yn = np.meshgrid(self.x, self.y)
+        Grav_sphere.Calced = True
+
+    def calc_fld(self):
+        """
+        Непосредственно расчет гравитационного поля шара
+        на основе gravity_field_vector(center, point)
+        """
+        print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+
+        gravity_field_vector(center, point):
+
+    def visu_fld(self, iscalced=Calced):
+        """
+        Визуализация расчитанного гравитационного поля шара
+        """
+        print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+        if not iscalced: self.calc_fld()
+
+
+        n_iso = 15  # num isolines
+        fig =plt.figure(figsize=(10,8))
+        # curves1 = plt.contourf(x,y, F, n_iso)
+        # curves3 = plt.contour(x, y, F, n_iso, colors='k')
+        #
+        # fig.colorbar(curves1)plt.xlabel('X')plt.ylabel('Y')plt.title('Sphere function')plt.show()
+        pass
+
+
+
 if __name__=="__main__":
-    tst_gravity_field()
-    tst_gravity_field_vector()
+    # tst_gravity_field()
+    # tst_gravity_field_vector()
+    # big_tst()
+    gr1=Grav_sphere()
+    gr1.print_ini_param()
+    gr1.make_coo()
+    gr1.calc_fld()
