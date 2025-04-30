b10a0fa55fc2c8e4b1de1792855f87f2f643ffb5
Ivan Genik <IVGenik@rambler.ru>
2025-04-30 19:28:34 +0500

    30/04/2025

--- gr_field.py
+++ gr_field.py
@@ -1,6 +1,10 @@
 """
 https://heybro.ai
 расчет гравитационного поля внутри и снаружи шара:
+Все работает правильно.
+Проверка формул:
+Потенциальные геофизические поля в задачах геологии_Ладынин (2008).pdf
+с.36-37 (20)
 """
 import sys
 
@@ -12,7 +16,7 @@ from pgreek_symbols import up_gr_symb
 # Константы
 G = 6.67430e-11  # гравитационная постоянная, м^3/(кг·с^2)
 M = 5.972e24     # масса Земли, кг
-R = 6_371_000      # радиус Земли, м
+R = 6_371_000.0      # радиус Земли, м
 GM=G*M
 R3=R**3
 
@@ -66,13 +70,13 @@ def gravity_field2(r):
 
 def tst_gravity_field():
     # Пример использования
-    distance = 10_000_000 #e7  # расстояние от центра, м
+    distance = 30_000_000.0 #e7  # расстояние от центра, м
     g_value = gravity_field(distance)
     print(f"Гравитационное поле на расстоянии {distance} м от центра Земли: {g_value} м/с^2")
     g_value = gravity_field2(distance)
     print(f"Гравитационное поле на расстоянии {distance} м от центра Земли: {g_value} м/с^2")
     #-------------------
-    radius=np.arange(0,distance,1000)
+    radius=np.arange(0,distance,10000.0)
     dg=np.empty_like(radius)
     for i, radi in enumerate(radius):
         print(i, radi)
@@ -81,13 +85,10 @@ def tst_gravity_field():
         dg[i]=dg_
     # dg = [gravity_field(r) for r in radius]
     # перевод в км
-    rad_km=radius/1000
+    rad_km=radius/1000.0
     plt.plot(rad_km, dg)
     plt.xlabel('Расстояние от центра Земли, км')
     plt.grid();  plt.show()
-
-
-
 
 if __name__=="__main__":
     # tst_gravity_field()
--- gr_spr_geof_1990.py
+++ gr_spr_geof_1990.py
@@ -7,6 +7,7 @@
 import numpy as np
 from math import pi, sqrt
 import matplotlib.pyplot as plt
+import inspect
 
 def input_data():
     """
@@ -23,7 +24,7 @@ def input_data():
     xc=0 # метры
     dx=1 # метры
     n=801
-    sig=1000 #  г/см**3 (1 кг/м**3)
+    sig=1000 # 1000 кг/м**3 = 1 г/см**3
     r = 100 # метры
     h = 200 # метры
     grc = 6.67430e-11  # гравитационная постоянная, м^3/(кг·с^2)
@@ -55,6 +56,7 @@ def calc_sharik(x0, xc, dx, n, sig, r, h, grc):
 
 def calc_spr(x0, xc, dx, n, sig, r, h, grc):
     """
+    Справочник геофизика. Гравиразведка. с.256-257 (129)
     x0 - начальная точка х0
     xc - x центра шара
     dx - шаг между точками
@@ -66,17 +68,32 @@ def calc_spr(x0, xc, dx, n, sig, r, h, grc):
     nt=[0]*n
     xt=[0]*n
     dg=[0]*n
-    m = 4 / 3 * pi * r * r * r * sig #   масса шара
+    m = (4 / 3) * pi * r**3 * sig #   масса шара
     GM=grc*m
     x = x0
     h2=h ** 2
     for k in range(n):
-        r=sqrt((x-xc)**2 + h2)
         nt[k]=k
         xt[k]=x
+        r=sqrt((x-xc)**2 + h2)
         dg[k]=GM*h/(r**3)
         x=x+dx
     return xt, dg # x координата, аномальное поле
+
+def visu_spr():
+    x0, xc, dx, n, sig, r, h, grc = input_data()
+    xts, dgs  = calc_spr(x0, xc, dx, n, sig, r, h, grc)
+    plt.figure(figsize=(12,8))
+    plt.subplot(1, 2, 1)
+    plt.title('Справочник геофизика, м/c**2')
+    plt.plot(xts, dgs)
+    plt.grid()
+
+    plt.subplot(1, 2, 2)
+    plt.title('Справочник геофизика, мГал')
+    plt.plot(xts, np.array(dgs)*10**5)
+    plt.grid(); plt.show()
+
 
 def visu_sharik():
     x0, xc, dx, n, sig, r, h, grc = input_data()
@@ -107,8 +124,37 @@ def visu_sharik():
     plt.grid()
     plt.show()
 
+def tst_spec_case():
+    """
+    Гравиразведка. Справочник геофизика (1990).djvu
+    с.258 (130)
+    """
+    print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+    grc = 6.67430e-11  # гравитационная постоянная, м^3/(кг·с^2)
+    r = 100 # м
+    sigma = 1000 # кг/м**3
+    M=(4/3)*pi*r*r*r*sigma
+    dzeta=200 # м
+    res_si=grc*M/dzeta**2
+    res_gr=res_si*10**5
+    print(f'{res_si=}  {res_gr=}')
+
+def calc_grc():
+    """
+    Гравитационная разведка Учебник для техникумов_Грушинский (1988).djvu
+    стр.6-7 (7-8)
+    """
+    print(f"\nFunction = {inspect.currentframe().f_code.co_name}")  # Вывод имени функции
+    M=6*10**24
+    R=6_371_000.0 # м
+    g=9.81
+    grc=g*R**2/M
+    print(f'{grc=}')
 
 if __name__=="__main__":
-    visu_sharik()
+    # visu_spr()
+    tst_spec_case()
+    calc_grc()
+    # visu_sharik()
 
 
